9496c316cd78a363af36b2917b03196527a554ba
Lucas Bäckman <lucas.backman@gmail.com>
2022-05-26 08:24:59 +0300

    Delete redundant selfs

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -13,7 +13,7 @@
 
 module Enumerable
   def my_each_with_index
-    return self.to_enum(__method__) { size if size } unless block_given?
+    return to_enum(__method__) { size if size } unless block_given?
 
     idx = 0
     my_each do |el|
@@ -24,42 +24,42 @@ module Enumerable
   end
 
   def my_select
-    return self.to_enum(__method__) { size if size } unless block_given?
+    return to_enum(__method__) { size if size } unless block_given?
 
     selected = []
-    self.my_each { |el| selected << el if yield(el) }
+    my_each { |el| selected << el if yield(el) }
     selected
   end
 
   def my_all?
     unless block_given?
-      return true if self.size == 0
+      return true if size == 0
 
       true_arr = []
-      self.my_each { |el| true_arr << el if el }
-      return true if true_arr.size == self.size
+      my_each { |el| true_arr << el if el }
+      return true if true_arr.size == size
 
       return false
     end
     true_arr = []
-    self.my_each { |el| true_arr << yield(el) if yield(el) }
-    return true if true_arr.size == self.size
+    my_each { |el| true_arr << yield(el) if yield(el) }
+    return true if true_arr.size == size
 
     false
   end
 
   def my_any?
     unless block_given?
-      return false if self.size == 0
+      return false if size == 0
 
       true_count = 0
-      self.my_each { |el| true_count += 1 if el }
+      my_each { |el| true_count += 1 if el }
       return true if true_count > 0
 
       return false
     end
     true_count = 0
-    self.my_each { |el| true_count += 1 if yield(el) }
+    my_each { |el| true_count += 1 if yield(el) }
     return true if true_count > 0
 
     false
@@ -69,17 +69,17 @@ module Enumerable
   # there is a truthy value, the method will return false.
   def my_none?
     unless block_given?
-      return true if self.size == 0
+      return true if size == 0
 
       true_arr = []
-      self.my_each do |el|
+      my_each do |el|
         true_arr << el if el
         return false if true_arr.size > 0
       end
       return true
     end
     true_arr = []
-    self.my_each do |el|
+    my_each do |el|
       true_arr << yield(el) if yield(el)
       return false if true_arr.size > 0
     end
@@ -87,18 +87,18 @@ module Enumerable
   end
 
   def my_count
-    return self.size unless block_given?
+    return size unless block_given?
 
     counted = 0
-    self.my_each { |el| counted += 1 if yield(el) }
+    my_each { |el| counted += 1 if yield(el) }
     counted
   end
 
   def my_map
-    return self.to_enum(__method__) { size if size } unless block_given?
+    return to_enum(__method__) { size if size } unless block_given?
 
     mapped = []
-    self.my_each { |el| mapped << yield(el) }
+    my_each { |el| mapped << yield(el) }
     mapped
   end
 
@@ -107,29 +107,29 @@ module Enumerable
       case args.size
       when 1
         sym = args.first
-        accumulator = self.shift
+        accumulator = shift
       when 2
         accumulator = args.first
         sym = args.last
       end
-      self.my_each { |el| accumulator = accumulator.send(sym, el) }
+      my_each { |el| accumulator = accumulator.send(sym, el) }
       return accumulator
     end
     case args.size
-    when 0 then accumulator = self.shift
+    when 0 then accumulator = shift
     when 1 then accumulator = args.first
     end
-    self.my_each { |el| accumulator = yield(accumulator, el) }
+    my_each { |el| accumulator = yield(accumulator, el) }
     accumulator
   end
 end
 
 class Array
   def my_each
-    return self.to_enum(__method__) { size if size } unless block_given?
-    
+    return to_enum(__method__) { size if size } unless block_given?
+
     i = 0
-    while i < self.size
+    while i < size
       yield(self[i])
       i += 1
     end
